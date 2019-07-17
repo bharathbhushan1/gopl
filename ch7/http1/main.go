fe45c2a6fe1f7f28b184fca9d1b5a51a7de971a9
Bharath Bhushan <bharath.bhushan.id@gmail.com>
2019-07-17 15:32:31 +0530

    handling different urls separately in http1 program

--- ch7/http1/main.go
+++ ch7/http1/main.go
@@ -20,7 +20,22 @@ func (d dollars) String() string {
 type database map[string]dollars
 
 func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	for i, p := range db {
-		fmt.Fprintf(w, "ITEM:%s PRICE:%s\n", i, p)
+	switch req.URL.Path {
+	case "/list":
+		for i, p := range db {
+			fmt.Fprintf(w, "%s: %s\n", i, p)
+		}
+	case "/price":
+		i := req.URL.Query().Get("item")
+		price, ok := db[i]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "item not found %q", i)
+			return
+		}
+		fmt.Fprintf(w, "%s: %s\n", i, price)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such page %s", req.URL)
 	}
 }
