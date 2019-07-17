a59fb16ef2125e65ea412edfd11853caa90eb1c0
Bharath Bhushan <bharath.bhushan.id@gmail.com>
2019-07-17 18:48:48 +0530

    clock1: simple multithreaded clock server

---
+++ ch8/clock1/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatalf("listen failed: %s", err)
+	}
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "accept failed: %s", err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().String())
+		if err != nil {
+			return
+		}
+		_, err = io.WriteString(c, "\n")
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
